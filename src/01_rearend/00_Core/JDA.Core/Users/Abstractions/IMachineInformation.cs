7be5af2d704ee44695131a7964b6e3afaf4cbbcb
BigBioMing <wocxming@sina.com>
2023-07-27 18:49:52 +0800

    添加系统信息

---
+++ src/01_rearend/00_Core/JDA.Core/Users/Abstractions/IMachineInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDA.Core.Users.Abstractions
+{
+    /// <summary>
+    /// 机器信息
+    /// </summary>
+    public interface IMachineInformation
+    {
+        public string HostName { get; }
+    }
+}
---
+++ src/01_rearend/00_Core/JDA.Core/Users/Distinguish/OSType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDA.Core.Users.Distinguish
+{
+    /// <summary>
+    /// 操作系统类型
+    /// </summary>
+    public enum OSType
+    {
+        /// <summary>
+        /// Windows
+        /// </summary>
+        [Description("Windows")]
+        Windows = 0,
+        /// <summary>
+        /// Linux
+        /// </summary>
+        [Description("Linux")]
+        Linux = 1
+    }
+}
