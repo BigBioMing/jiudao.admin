a9566c6437856d57e8d9cf45e1d53a1444f2043a
BigBioMing <wocxming@sina.com>
2023-08-10 19:29:26 +0800

    111

--- src/01_rearend/00_Core/JDA.Core.WebApi/ApiDocs/ApiVersionAttribute.cs
+++ src/01_rearend/00_Core/JDA.Core.WebApi/ApiDocs/ApiVersionAttribute.cs
@@ -11,13 +11,24 @@ namespace JDA.Core.WebApi.ApiDocs
     /// API版本
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
-    public class ApiVersionAttribute : Attribute, IApiDescriptionGroupNameProvider
+    public class ApiVersionAttribute : Attribute, IApiDescriptionGroupNameProvider, IApiDescriptionVisibilityProvider
     {
+        private ApiVersionDefine _version;
         public ApiVersionAttribute()
         {
-
+            this.IgnoreApi = false;
         }
         public string GroupName { get; set; }
-        public ApiVersionDefine Version { get; set; }
+        public ApiVersionDefine Version
+        {
+            get => this._version;
+            set
+            {
+                this._version = value;
+                this.GroupName = this._version.ToString();
+            }
+        }
+
+        public bool IgnoreApi { get; set; }
     }
 }
