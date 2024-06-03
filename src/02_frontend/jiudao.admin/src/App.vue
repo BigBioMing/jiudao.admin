1b605355a3c4bfca109c4372bbd9efc920075a6a
xiaoming.cheng <xiaoming.cheng>
2024-06-03 16:33:47 +0800

    添加左侧菜单

--- src/02_frontend/jiudao.admin/src/App.vue
+++ src/02_frontend/jiudao.admin/src/App.vue
@@ -1,38 +1,149 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-} from '@ant-design/icons-vue';
+import { reactive, ref,watch,h,createVNode,render ,createCommentVNode, onMounted} from 'vue';
+// import {
+//   MenuUnfoldOutlined,
+//   MenuFoldOutlined,
+// } from '@ant-design/icons-vue';
 
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+  MailOutlined,
+  DesktopOutlined,
+  InboxOutlined,
+  AppstoreOutlined,
+} from '@ant-design/icons-vue';
 console.log(import.meta.env)
 const selectedKeys = ref<string[]>(['1']);
 const collapsed = ref<boolean>(false);
+
+const state = reactive({
+  collapsed: false,
+  selectedKeys: ['1'],
+  openKeys: ['sub1'],
+  preOpenKeys: ['sub1'],
+});
+let vn1 = createVNode('PieChartOutlined');
+console.log(vn1)
+onMounted(()=>{
+  render(vn1,document.getElementById('xxx') as HTMLElement)
+})
+const items = reactive([
+  {
+    key: '1',
+    // icon: () => h(PieChartOutlined),
+    icon:  () => import(`@ant-design/icons-vue/PieChartOutlined`),
+    // icon:  (pa:any)=>{
+    //   console.log(pa);
+    //   return vn1
+    // },
+    label: 'Option 1',
+    title: 'Option 1',
+  },
+  {
+    key: '2',
+    icon: () => h(DesktopOutlined),
+    label: 'Option 2',
+    title: 'Option 2',
+  },
+  {
+    key: '3',
+    icon: () => h(InboxOutlined),
+    label: 'Option 3',
+    title: 'Option 3',
+  },
+  {
+    key: 'sub1',
+    icon: () => h(MailOutlined),
+    label: 'Navigation One',
+    title: 'Navigation One',
+    children: [
+      {
+        key: '5',
+        label: 'Option 5',
+        title: 'Option 5',
+      },
+      {
+        key: '6',
+        label: 'Option 6',
+        title: 'Option 6',
+      },
+      {
+        key: '7',
+        label: 'Option 7',
+        title: 'Option 7',
+      },
+      {
+        key: '8',
+        label: 'Option 8',
+        title: 'Option 8',
+      },
+    ],
+  },
+  {
+    key: 'sub2',
+    icon: () => h(AppstoreOutlined),
+    label: 'Navigation Two',
+    title: 'Navigation Two',
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+        title: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+        title: 'Option 10',
+      },
+      {
+        key: 'sub3',
+        label: 'Submenu',
+        title: 'Submenu',
+        children: [
+          {
+            key: '11',
+            label: 'Option 11',
+            title: 'Option 11',
+          },
+          {
+            key: '12',
+            label: 'Option 12',
+            title: 'Option 12',
+          },
+        ],
+      },
+    ],
+  },
+]);
+watch(
+  () => state.openKeys,
+  (_val, oldVal) => {
+    state.preOpenKeys = oldVal;
+  },
+);
+const toggleCollapsed = () => {
+  state.collapsed = !state.collapsed;
+  state.openKeys = state.collapsed ? [] : state.preOpenKeys;
+};
 </script>
 
 <template>
+  <div>
+    hhh
+    <div id="xxx"></div>
+  </div>
   <a-layout>
-    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+    <a-layout-sider v-model:collapsed="state.collapsed" :trigger="null" collapsible>
       <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <user-outlined />
-          <span>nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <video-camera-outlined />
-          <span>nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <upload-outlined />
-          <span>nav 3</span>
-        </a-menu-item>
-      </a-menu>
+      <a-menu v-model:openKeys="state.openKeys" v-model:selectedKeys="state.selectedKeys" mode="inline" theme="dark"
+        :inline-collapsed="state.collapsed" :items="items"></a-menu>
     </a-layout-sider>
     <a-layout>
       <a-layout-header style="background: #fff; padding: 0">
-        <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
-        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+        <menu-unfold-outlined v-if="collapsed" class="trigger" @click="toggleCollapsed" />
+        <menu-fold-outlined v-else class="trigger" @click="toggleCollapsed" />
       </a-layout-header>
       <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
         Content
--- src/02_frontend/jiudao.admin/src/main.ts
+++ src/02_frontend/jiudao.admin/src/main.ts
@@ -1,10 +1,23 @@
 import Antd from 'ant-design-vue';
-import AntdIcons from '@ant-design/icons-vue';
+import * as AntdIcons from '@ant-design/icons-vue';
+console.log('AntdIcons', AntdIcons);
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'ant-design-vue/dist/reset.css';
-
 const app = createApp(App);
 
-app.use(Antd).mount('#app');
+app.use(Antd);
+
+let a: {
+    [key in keyof typeof app]: any
+}
+for (let key in AntdIcons) {
+    //  console.log('key',(AntdIcons as any)[key]);
+    //  console.log('key',(<any>AntdIcons)[key]);
+    // app.component(key, (AntdIcons as any)[key]);
+    app.component(key, AntdIcons[key as keyof typeof AntdIcons]);
+}
+console.log('typeof(AntdIcons)', typeof (AntdIcons));
+
+app.mount('#app');
